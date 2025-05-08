7a326e4dcbbfd8d408f49924fbfa448809a2548f
AjithS <ajith@datacaliper.com>
2025-05-08 15:22:48 +0530

    build bug fixing

--- src/app/practice-listings/page.tsx
+++ src/app/practice-listings/page.tsx
@@ -2,39 +2,9 @@ import PageHeader from "@/components/practice-listings/page-header"
 import SearchFilters from "@/components/practice-listings/search-filters"
 import ListingsSection from "@/components/sections/listings-section"
 import ContactSection from "@/components/sections/contact-section"
-import { fetchListings } from "@/services/pediatric-service"
-// import { headers } from "next/headers"
 
-// export default async function PracticeListingsPage() {
-
-// const headersList = headers()
-// const fullUrl = headersList.get("x-url") || ""
-// const url = new URL(fullUrl || "http://localhost") // fallback base for local dev
-
-//       const params = url.searchParams
-//   const filters = {
-//     country: params.get("country") || "",
-//     state: params.get("state") || "",
-//     industrie: params.get("industrie") || "",
-//     minPrice: params.get("minPrice") || "",
-//     maxPrice: params.get("maxPrice") || "",
-//     page: parseInt(params.get("page") || "1", 10),
-//   }
-
-//   const { listings, count } = await fetchListings(filters)
-
-//   return (
-//     <div className="min-h-screen">
-//       <PageHeader />
-//       <SearchFilters />
-//       <ListingsSection initialListings={listings} totalCount={count} filters={filters} />
-//       <ContactSection />
-//     </div>
-//   )
-// }
-
-
-export default async function PracticeListingsPage({ searchParams }: { searchParams: Record<string, string> }) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function PracticeListingsPage({ searchParams }: { searchParams?: any }) {
   const filters = {
     country: searchParams?.country || "",
     state: searchParams?.state || "",
@@ -46,13 +16,13 @@ export default async function PracticeListingsPage({ searchParams }: { searchPar
 
   console.log("Filters passed to fetchListings:", filters) // Debugging
 
-  const { listings, count } = await fetchListings(filters)
+  // const { listings, count } = await fetchListings(filters)
 
   return (
     <div>
       <PageHeader />
       <SearchFilters />
-      <ListingsSection initialListings={listings} totalCount={count} filters={filters} />
+      <ListingsSection filters={filters} />
       <ContactSection />
     </div>
   )
